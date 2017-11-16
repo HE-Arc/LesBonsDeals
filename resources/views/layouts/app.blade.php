b34aef8634449819aa3b610c8ab3acecd38a43ab
André Neto da Silva <andre.netodasilva@he-arc.ch>
2017-11-16 23:43:46 +0100

    Add images example of one article. Fixed navbar on top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+    <nav class="navbar sticky-top navbar-expand-md navbar-dark bg-primary">
         <a class="navbar-brand" href="{{route('index')}}">LesBonsDeals</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navContent"
                 aria-controls="navContent" aria-expanded="false" aria-label="Toggle navigation">
