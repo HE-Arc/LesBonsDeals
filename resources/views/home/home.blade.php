473d7155e9945a34531abbd6507a23f3a9045478
André Neto da Silva <andre.netodasilva@he-arc.ch>
2017-11-30 12:55:25 +0100

    menu selected is now active when selected

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -8,16 +8,33 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="list-group" id="list-tab" role="tablist">
-
-                            <a class="list-group-item list-group-item-action active" id="list-home-list"
+                            <a class="list-group-item list-group-item-action
+@if(Route::currentRouteName() === 'home')
+                                    active
+@endif
+                                    " id="list-home-list"
                                href="{{route('home')}}" role="tab" aria-controls="home">Informations
                                 personnelles</a>
-                            <a class="list-group-item list-group-item-action" id="list-home-list"
+                            <a class="list-group-item list-group-item-action
+@if(Route::currentRouteName() === 'manage_articles')
+                                    active
+@endif
+                                    " id="list-home-list"
                                href="{{route('manage_articles')}}" role="tab" aria-controls="home">Articles mis en
                                 vente</a>
-                            <a class="list-group-item list-group-item-action" id="list-home-list"
+
+                            <a class="list-group-item list-group-item-action
+
+@if(Route::currentRouteName() === 'article.create')
+                                    active
+@endif" id="list-home-list"
                                href="{{route('article.create')}}" role="tab" aria-controls="home">Vendre un article</a>
-                            <a class="list-group-item list-group-item-action" id="list-home-list"
+                            <a class="list-group-item list-group-item-action
+@if(Route::currentRouteName() === 'settings')
+                                    active
+@endif
+                                    " id="list-home-list"
+
                                href="{{route('settings')}}" role="tab" aria-controls="home">Gestion du
                                 compte</a>
                         </div>
